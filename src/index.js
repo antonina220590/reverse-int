d9094e2034442922d6fffa9c40995f6ee5971cf6
antonina220590 <tonya2290@gmail.com>
2023-05-16 14:13:39 +0300

    feat: do reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+
+
+if(n > 0) {
+
+return n.toString().split('').reverse().join('')
+
 }
+
+if(n < 0) {
+  return Math.abs(n).toString().split('').reverse().join('')
+}
+
+}
+
